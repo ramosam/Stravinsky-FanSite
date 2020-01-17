75eaad0590a6f8097ea0c7d8151c95843d0520cb
Alicia Ramos <ramosam@my.lanecc.edu>
2020-01-17 10:36:42 -0800

    Beginning to integrate project files into new project.

---
+++ ArchivedProject/StravinskyArchive/StravinskyFanSite/Repositories/Repository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StravinskyFanSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StravinskyFanSite.Repositories
+{
+    public class Repository : IRepository
+    {
+
+        private AppDbContext context;
+
+        public List<UserStory> UserStories
+        {
+            get
+            {
+                var userStories = context.UserStories
+                    .Include(story => story.Comments)
+                    .ToList();
+
+                return userStories;
+            }
+        }
+
+        public Repository(AppDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public void AddUserStory(UserStory userStory)
+        {
+            context.UserStories.Add(userStory);
+            context.SaveChanges();
+        }
+
+        public void AddComment(UserStory uS, Comment comment)
+        {
+            context.Comments.Add(comment);
+            uS.Comments.Add(comment);
+            context.UserStories.Update(uS);
+            context.SaveChanges();
+        }
+
+        public UserStory GetUserStoryByPost(string storypost)
+        {
+            UserStory uStory = context.UserStories.FirstOrDefault(u => u.StoryPost == storypost);
+            return uStory;
+        }
+
+        public UserStory GetUserStoryByUser(string user)
+        {
+            UserStory uStory = context.UserStories.FirstOrDefault(u => u.Name == user);
+            return uStory;
+        }
+
+        public void SortUserStoryByTitle()
+        {
+            context.UserStories.OrderBy(userStory => userStory.StoryPost);
+        }
+
+        public void SortUserStoryByUserName()
+        {
+            context.UserStories.OrderBy(userStory => userStory.Name);
+        }
+
+    }
+}
--- ArchivedProject/StravinskyArchive/StravinskyFanSite/Startup.cs
+++ ArchivedProject/StravinskyArchive/StravinskyFanSite/Startup.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using StravinskyFanSite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using StravinskyFanSite.Models;
+using StravinskyFanSite.Data;
+using StravinskyFanSite.Repositories;
 
 namespace StravinskyFanSite
 {
@@ -27,13 +24,27 @@ namespace StravinskyFanSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddMvc();
+
+            services.AddTransient<IRepository, Repository>();
+
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlite(
+                    Configuration["ConnectionStrings:SQLiteConnection"]));
+
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
