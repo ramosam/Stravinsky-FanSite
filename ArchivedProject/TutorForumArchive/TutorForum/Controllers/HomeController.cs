1d42cdfbc0ec6034f991ccbc898a544a561fea99
Alicia Ramos <aliciaramos@icloud.com>
2020-01-16 19:58:02 -0800

    Recreating Fansite project on Windows.  Archiving original Fansite.

---
+++ ArchivedProject/TutorForumArchive/TutorForum/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TutorForum.Models;
+using TutorForum.Repositories;
+
+namespace TutorForum.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        IRepository repo;
+        public HomeController(IRepository r)
+        {
+            repo = r;
+        }
+
+        // Set up home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Set up About page
+        public IActionResult About()
+        {
+            List<Tutor> tutors = repo.Tutors;
+            return View(tutors);
+        }
+
+
+        // Bump the Privacy page down
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
